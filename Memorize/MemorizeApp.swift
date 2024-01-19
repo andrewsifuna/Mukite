81197bbf96f8daad3c450414f19d9feba85cbe87
andrewsifuna <38553010+andrewsifuna@users.noreply.github.com>
2024-01-19 21:07:42 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andrew Sifuna on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
